997d31369ca32a6ce288dd067f0043049f038e62
iamspsarathy <35688629+iamspsarathy@users.noreply.github.com>
2018-03-12 15:51:48 +0530

    Create 61.C

---
+++ 61.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() 
+
+{
+char a[100];
+int n,i,num;
+
+scanf("%s",a);
+scanf("%d",&num);
+
+n=strlen(a);
+for(i=0;i<num;i++)
+
+    {
+
+        printf("%c",a[i]);
+
+    }
+
+	return 0;
+
+}
+© 2018 GitHub, Inc.
